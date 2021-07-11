3e45c76073c13ead5e7a8495be369a1a82c09887
MRW Neundorf <ninevillage@gmail.com>
2021-07-11 12:46:52 +0200

    several updates

--- main.go
+++ main.go
@@ -1,20 +1,48 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"os"
+    "flag"
+    "log"
+    "net/http"
+    "time"
 
-	"github.com/gorilla/mux"
+    "github.com/gorilla/mux"
 )
 
 func main() {
-	port := os.Getenv("PORT")
+    var entry string
+    var static string
 
-	if port == "" {
-		port = "8080"
-	}
+    var host string
+    var port string
+
+    flag.StringVar(&entry, "entry", "./client/build/index.html", "the entrypoint to serve.")
+    flag.StringVar(&static, "static", "./client/build/", "the directory to serve static files from.")
+    flag.StringVar(&port, "host", "", "the `host` to listen on.")
+    flag.StringVar(&port, "port", "8000", "the `port` to listen on.")
+    flag.Parse()
 
 	r := mux.NewRouter()
-	log.Fatal(http.ListenAndServe(":"+port, r))
+
+    // This will serve files under http://localhost:8000/static/<filename>
+    r.PathPrefix("/").Handler(http.StripPrefix("/", http.FileServer(http.Dir(static))))
+
+	server := &http.Server{
+	    Handler: r,
+	    Addr: host+":"+port,
+
+	    WriteTimeout: 15 * time.Second,
+	    ReadTimeout: 15 * time.Second,
+    }
+
+	log.Print("listening on ", server.Addr)
+	log.Fatal(server.ListenAndServe())
+}
+
+func IndexHandler(entryPoint string) func(w http.ResponseWriter, r *http.Request) {
+    fn := func(w http.ResponseWriter, r *http.Request) {
+        http.ServeFile(w, r, entryPoint)
+    }
+
+    return fn
 }
